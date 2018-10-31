c84765d839497447fd202f48eae18f5af7075362
Jordy Manner <jordy.manner@milkcreation.fr>
2018-10-31 18:08:45 +0000

    static-assets -- tag 2.0.1 -- réorga tiFy

---
+++ Contracts/ServerController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace tiFy\Plugins\StaticAssets\Contracts;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use tiFy\Contracts\Kernel\ParamsBag;
+
+interface ServerController extends ParamsBag, ServerResolver
+{
+    /**
+     * Traitement de l'affichage de la ressource.
+     *
+     * @param ServerRequestInterface $request Requête serveur valide PSR.
+     * @param ResponseInterface $response Réponse serveur valide PSR.
+     * @param array $args Liste des variables passées en arguments.
+     *
+     * @return string
+     */
+    public function __invoke(ServerRequestInterface $request, ResponseInterface $response, $args);
+}
