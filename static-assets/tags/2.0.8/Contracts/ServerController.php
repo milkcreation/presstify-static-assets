c610c9d919994b9aa88f24df6ad8d9aa77d316d3
mastermilk <mastermilk@9c4951e9-fce0-42f0-91e9-f5562070ff10>
2019-06-15 10:25:49 +0000

    static-assets -- tag 2.0.8 -- ajustement composer glide

---
+++ static-assets/tags/2.0.8/Contracts/ServerController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace tiFy\Plugins\StaticAssets\Contracts;
+
+use Psr\Http\Message\ServerRequestInterface;
+use tiFy\Contracts\Kernel\ParamsBag;
+
+interface ServerController extends ParamsBag, ServerResolver
+{
+    /**
+     * Traitement de l'affichage de la ressource.
+     *
+     * @param ServerRequestInterface $request Requête serveur valide PSR.
+     * @param array $args Liste des variables passées en arguments.
+     *
+     * @return string
+     */
+    public function __invoke(ServerRequestInterface $request, $args);
+}
---
+++ static-assets/tags/2.0.8/Server/ServerImgController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace tiFy\Plugins\StaticAssets\Server;
+
+use League\Glide\Filesystem\FileNotFoundException;
+use League\Glide\ServerFactory;
+use League\Glide\Signatures\SignatureException;
+use League\Flysystem\Filesystem;
+use League\Flysystem\Memory\MemoryAdapter;
+use Psr\Http\Message\ServerRequestInterface;
+use Zend\Diactoros\Response;
+
+class ServerImgController extends ServerAbstractController
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function __invoke(ServerRequestInterface $request, $args)
+    {
+        $response = new Response();
+
+        $path = $args['path'] ?? null;
+
+        $server = ServerFactory::create([
+            'source' => $this->get('source') ? : dirname(__DIR__) . '/Resources/source',
+            'cache'  => $this->get('cache') ? : new Filesystem(new MemoryAdapter())
+        ]);
+
+        if ($secure = $this->get('secure')) :
+            try {
+                ($this->signature($secure))
+                    ->validateRequest(
+                        $path,
+                        $request->getQueryParams()
+                    );
+                $server->outputImage($path, $request->getQueryParams());
+            } catch (SignatureException $e) {
+                $response->getBody()->write($e->getMessage());
+            }
+        else :
+            try {
+                $server->outputImage($path, $request->getQueryParams());
+            } catch (FileNotFoundException $e) {
+                $this->get('debug', false)
+                    ? $response->getBody()->write($e->getMessage())
+                    : $server->outputImage($this->get('holder'), $request->getQueryParams());
+            }
+        endif;
+
+        return $response;
+    }
+}
