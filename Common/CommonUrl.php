62ac5bdeca8aaf63f7aeaea0fd080c3aa1c16545
Jordy Manner <jordy.manner@milkcreation.fr>
2019-09-19 07:57:36 +0000

    static-assets -- tag 2.0.11 -- Correctif mineur

---
+++ Common/CommonUrl.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace tiFy\Plugins\StaticAssets\Common;
+
+use League\Glide\Urls\UrlBuilder;
+use tiFy\Plugins\StaticAssets\Contracts\CommonUrl as CommonUrlContract;
+
+class CommonUrl extends UrlBuilder implements CommonUrlContract
+{
+
+}
