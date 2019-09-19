fd5331a0292daeaf412b69f55e6e8d08db5639e6
mastermilk <mastermilk@9c4951e9-fce0-42f0-91e9-f5562070ff10>
2019-09-19 07:57:36 +0000

    static-assets -- tag 2.0.11 -- Correctif mineur

---
+++ static-assets/tags/2.0.11/Server/ServerManager.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace tiFy\Plugins\StaticAssets\Server;
+
+use League\Container\Container;
+use League\Route\Http\Exception\NotFoundException;
+use League\Route\Http\Exception\MethodNotAllowedException;
+use tiFy\Plugins\StaticAssets\Common\CommonServiceProvider;
+use tiFy\Plugins\StaticAssets\Contracts\ServerManager as ServerManagerContract;
+use Zend\HttpHandlerRunner\Exception\EmitterException;
+
+class ServerManager extends Container implements ServerManagerContract
+{
+    use ServerResolver;
+
+    /**
+     * Instance du contrôleur du gestionnaire de ressources.
+     * @var ServerManager
+     */
+    protected $app;
+
+    /**
+     * CONSTRUCTEUR.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->app = $this;
+
+        $this->addServiceProvider(new CommonServiceProvider());
+        $this->addServiceProvider(new ServerServiceProvider());
+
+        foreach (['css', 'img', 'js'] as $type) :
+            foreach ($this->params($type, []) as $path => $attrs) :
+                $this->route($type, 'GET', $path, $this->resolve("server.controller.{$type}", [$this, $attrs]));
+            endforeach;
+        endforeach;
+
+        try {
+            $response = $this->router()->dispatch($this->httpRequest());
+        } catch (NotFoundException $e) {
+            echo $e->getMessage();
+            die(500);
+        } catch (MethodNotAllowedException $e) {
+            echo $e->getMessage();
+            die(500);
+        }
+
+        if (isset($response)) :
+            try {
+                $emitter = $this->httpEmitter();
+                $emitter->emit($response);
+            } catch(EmitterException $e) {
+                echo $e->getMessage();
+                die(500);
+            }
+        endif;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function resolve($alias, $args = [])
+    {
+        return $this->get("assets.{$alias}", $args);
+    }
+}
