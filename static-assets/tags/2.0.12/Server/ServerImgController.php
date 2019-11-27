e518ef5d9e1bc1d724c97759cce639185e1d1fbf
mastermilk <mastermilk@9c4951e9-fce0-42f0-91e9-f5562070ff10>
2019-11-27 18:15:15 +0000

    static-assets -- tag 2.0.12 -- réécriture

---
+++ static-assets/tags/2.0.12/Server/ServerImgController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace tiFy\Plugins\StaticAssets\Server;
+
+use Exception;
+use League\Glide\ServerFactory;
+use League\Flysystem\Filesystem;
+use League\Flysystem\Memory\MemoryAdapter;
+use Psr\Http\Message\ServerRequestInterface;
+use Zend\Diactoros\Response;
+
+class ServerImgController extends ServerAbstractController
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function __invoke(ServerRequestInterface $request, $args)
+    {
+        $response = new Response();
+
+        $path = $args['path'] ?? null;
+
+        $server = ServerFactory::create([
+            'source' => $this->get('source') ? : dirname(__DIR__) . '/Resources/source',
+            'cache'  => $this->get('cache') ? : new Filesystem(new MemoryAdapter())
+        ]);
+
+        if ($secure = $this->get('secure')) {
+            try {
+                ($this->signature($secure))->validateRequest($path, $request->getQueryParams());
+                $server->outputImage($path, $request->getQueryParams());
+            } catch (Exception $e) {
+                $response->getBody()->write($e->getMessage());
+            }
+        } else {
+            try {
+                $server->outputImage($path, $request->getQueryParams());
+            } catch (Exception $e) {
+                $this->get('debug', false)
+                    ? $response->getBody()->write($e->getMessage())
+                    : $server->outputImage($this->get('placeholder'), $request->getQueryParams());
+            }
+        }
+
+        return $response;
+    }
+}
